8d3efd164098dae975526bb95889ac4ca6e6be26
Ayush Singh <ayush893singh@gmail.com>
2026-04-24 00:11:58 +0530

    Add files via upload

---
+++ QR_genrate.py
@@ -0,0 +1,61 @@
+import qrcode
+
+class QRGenerator:
+
+    def text_to_qr(self):
+        text = input("Enter text or message: ")
+
+        img = qrcode.make(text)
+        img.save("text_qr.png")
+
+        print("\nText QR saved as text_qr.png")
+
+    def website_to_qr(self):
+        url = input("Enter website URL (https://...): ")
+
+        img = qrcode.make(url)
+        img.save("website_qr.png")
+
+        print("\nWebsite QR saved as website_qr.png")
+
+    def upi_payment_qr(self):
+        print("\n===== AUTO PAY QR =====")
+
+        upi_id = input("Enter UPI ID : ")
+        name = input("Enter Payee Name: ")
+        amount = input("Enter Amount (Optional): ")
+
+        if amount == "":
+            upi_link = f"upi://pay?pa={upi_id}&pn={name}"
+        else:
+            upi_link = f"upi://pay?pa={upi_id}&pn={name}&am={amount}&cu=INR"
+
+        img = qrcode.make(upi_link)
+        img.save("upi_payment_qr.png")
+
+        print("\nUPI Payment QR saved as upi_payment_qr.png")
+
+    def menu(self):
+        while True:
+            print("\n===== QR GENERATOR MENU =====")
+            print("1. Text to QR")
+            print("2. Website to QR")
+            print("3. UPI Payment QR")
+            print("4. Exit")
+
+            choice = input("Enter choice: ")
+
+            if choice == "1":
+                self.text_to_qr()
+            elif choice == "2":
+                self.website_to_qr()
+            elif choice == "3":
+                self.upi_payment_qr()
+            elif choice == "4":
+                print("=== Bye ===")
+                break
+            else:
+                print("Invalid choice")
+
+qr = QRGenerator()
+qr.menu()
